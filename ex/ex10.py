0bd4f046f7479dc2c84ae494b0cc9a66d026b671
Rachelle Gray <msrachellegray@gmail.com>
2016-03-16 08:28:28 -0700

    Python the hard way ex 10 escape sequences

--- ex/ex10.py
+++ ex/ex10.py
@@ -12,4 +12,29 @@ I'll do a list:
 print tabby_cat
 print persian_cat
 print backslash_cat
-print fat_cat 
+print fat_cat
+
+##
+##	Python escape sequences
+# \\			Backslash (\)
+# \'			Single-quote (')
+# \"			Double-quote (")
+# \a			ASCII bell (BEL)
+# \b    		ASCII backspace (BS)
+# \f 			ASCII formfeed (FF)
+# \n 			ASCII linefeed (LF)
+# \N{name}		Character named name
+#				in the Unicode database			
+# 				(Unicode only)
+# \r			Carriage Return (CR)
+# \t 			Horizontal Tab (TAB)
+# \uxxxx		Character with 16-bit hex value xxxx
+#				(Unicode only)
+# \Uxxxxxxxx	Charcter with 32-bit
+# \v 			ASCII vertical tab (VT)
+# \ooo 			Character with octal value ooo
+# \xhh			Character with hex value hh
+# 
+# 
+# 
+# 
