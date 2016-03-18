6246bbfbe45fe3e4568ca750602ef717587ffebd
Rachelle Gray <msrachellegray@gmail.com>
2016-03-18 09:53:22 -0700

    Python exercise 11 - asking questions

---
+++ ex/ex11.py
@@ -0,0 +1,16 @@
+# exercise 11: Asking Questions
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." %(
+	age, height, weight)
+
+# Study drills:
+# 1. research raw_input
+# 2. What are other ways to use it? Copy, bio, etc.
+# 3. Write another form and ask more questions.
+# 4. what's with the escape sequence back slash?
